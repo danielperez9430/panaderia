f0ff0a0d26ee501c27fdc103b964f6f026792f56
Daniel Pérez Fernández <dpefe@hotmail.es>
2020-11-02 19:11:36 +0100

    Dynamic Product Page
    - Add producto.js data
    - Add links in Panaderia + Pasteleria

--- js/producto.js
+++ js/producto.js
@@ -1,23 +1,41 @@
-
+// Producto por defecto
 var producto = {
-    nombre : "Magdalenas de chocolate",
-    precio : "10",
-    descripcion : "Lorem adddddddddd adda ad daad adada ad ererereretwerggwgww twrwt  ewwewe ewe e  e e erer ere e er tw  e",
-    image : "/images/banner-1.webp"
+    nombre: "Magdalenas de chocolate",
+    precio: "10",
+    descripcion: "Lorem adddddddddd adda ad daad adada ad ererereretwerggwgww twrwt  ewwewe ewe e  e e erer ere e er tw  e",
+    image: "/images/banner-1.webp"
 }
 
 let obtenerUrlActual = window.location;
 
 var idURL = obtenerUrlActual.toString().split('?id=');
 
-if(idURL.length === 2) {
-    if(!isNaN(idURL[1])) {
+if (idURL.length === 2) {
+    if (!isNaN(idURL[1])) {
 
-        document.getElementById("name").innerText = producto.nombre;
-        document.getElementById("descripcion_text").innerText = producto.descripcion;
-        document.getElementsByClassName("precio_producto").item(0).innerHTML = "Precio: " + producto.precio + "€";
-        document.getElementsByClassName("img_producto").item(0).src = producto.image;
+        fetch('/data/productos.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.products[idURL[1]].price);
+
+                if(data.products.length >= idURL[1]) {
+                    
+                    producto.nombre = data.products[idURL[1]].productName;
+                    producto.precio = data.products[idURL[1]].price;
+                    producto.descripcion = data.products[idURL[1]].description;
+                    producto.image = data.products[idURL[1]].image;
+                }
+
+                document.getElementById("name").innerText = producto.nombre;
+                document.getElementById("descripcion_text").innerText = producto.descripcion;
+                document.getElementsByClassName("precio_producto").item(0).innerHTML = "Precio: " + producto.precio + "€";
+                document.getElementsByClassName("img_producto").item(0).src = producto.image;
+
+            });
+
+
 
     }
-    
+
 }
+
