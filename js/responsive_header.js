b3b036b32574193e8167152d4fac817d18c3b4f0
Juan <juangilsanz@gmail.com>
2020-11-16 22:27:56 +0100

    Added homeage products section

--- js/responsive_header.js
+++ js/responsive_header.js
@@ -15,9 +15,50 @@ const carrousel = {
     ]
 }
 
+const homepageProducts = {
+    categories: [
+        {
+            title: 'Ultimos productos',
+            products: [
+                {
+                    name: 'Pan 1',
+                    picture: 'assets/pan1.jpg',
+                    url: ''
+                },
+                {
+                    name: 'Pan 2',
+                    picture: 'assets/pan2.jpg',
+                    url: ''
+                },
+                {
+                    name: 'Pan 3',
+                    picture: 'assets/pan3.jpg',
+                    url: ''
+                },
+                {
+                    name: 'Pan 4',
+                    picture: 'assets/pan1.jpg',
+                    url: ''
+                },
+                {
+                    name: 'Pan 5',
+                    picture: 'assets/pan2.jpg',
+                    url: ''
+                },
+                {
+                    name: 'Pan 6',
+                    picture: 'assets/pan3.jpg',
+                    url: ''
+                },
+            ]
+        },
+    ]
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     menuManagement();
     createCarrousel();
+    createProducts();
 });
 
 function menuManagement() {
@@ -42,4 +83,25 @@ function createCarrousel() {
             `<div class="carousel-item ${i === 0 ? 'active' : ''}"> <img class="d-block w-100" src="${carrousel.items[i].picture}"> </div>`
         );
     }
+}
+
+function createProducts() {
+    homepageProducts.categories.forEach(category => {
+        const categoryDiv = document.createElement('div');
+        categoryDiv.setAttribute('class', 'category');
+        const title = document.createElement('h3');
+        title.setAttribute('class', 'categoryTitle');
+        const titleText = document.createTextNode(category.title);
+        title.appendChild(titleText);
+        categoryDiv.appendChild(title);
+        const productsContainer = document.createElement('div');
+        productsContainer.setAttribute('class', 'productsContainer');
+
+        category.products.forEach(product => {
+            $(productsContainer).append(`<div class="productContainer"><span class="productTitle">${product.name}</span><img src="${product.picture}"></div>`);
+        });
+
+        categoryDiv.appendChild(productsContainer);
+        document.getElementById('productsSection').appendChild(categoryDiv);
+    });
 }
