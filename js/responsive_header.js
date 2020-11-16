7a5bd1c0b533d258911eab2eff862b2833d24f89
Juan <juangilsanz@gmail.com>
2020-11-16 21:43:42 +0100

    Created carousel and some style improvements

--- js/responsive_header.js
+++ js/responsive_header.js
@@ -1,15 +1,45 @@
+const carrousel = {
+    items: [
+        {
+            picture: 'assets/picture1.jpg',
+            destinationURL: ''
+        },
+        {
+            picture: 'assets/picture2.jpg',
+            destinationURL: ''
+        },
+        {
+            picture: 'assets/picture3.jpg',
+            destinationURL: ''
+        },
+    ]
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    menuManagement();
+    createCarrousel();
+});
+
+function menuManagement() {
     var menuStatus = 'display';
     document.getElementById('hambButton').addEventListener('click', () => {
         if (menuStatus == 'hidden') {
             $('#hambMenu').removeClass('hideHambMenu');
-            $('#mainContent').removeClass('mainContentHiddenMenu');
+            $('#bodyContainer').removeClass('mainContentHiddenMenu');
             menuStatus = 'display';
         }
         else if (menuStatus == 'display') {
             $('#hambMenu').addClass('hideHambMenu');
-            $('#mainContent').addClass('mainContentHiddenMenu');
+            $('#bodyContainer').addClass('mainContentHiddenMenu');
             menuStatus = 'hidden';
         }
     });
-});
+}
+
+function createCarrousel() {
+    for (let i = 0; i < carrousel.items.length; i++) {
+        $('#carouselPictures').append(
+            `<div class="carousel-item ${i === 0 ? 'active' : ''}"> <img class="d-block w-100" src="${carrousel.items[i].picture}"> </div>`
+        );
+    }
+}
