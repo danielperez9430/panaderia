8650b2df5c4b582901ad5af4af23a898bb7f77e6
Daniel Pérez Fernández <dpefe@hotmail.es>
2020-12-12 20:27:08 +0100

    Mejoras en la Media Query por JS

--- js/responsive_header.js
+++ js/responsive_header.js
@@ -13,13 +13,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     /** Media Query en JS para modificar el DOM en este caso */
+
     var ventana = window;
+
+    var cambiar = true; // Variable bandera para que no se repitan acciones
 
     ventana.addEventListener('resize', function (mql) {
         var ancho_ventana = window.innerWidth; // Obtenemos el ancho actual de la ventana
 
+        if(ancho_ventana< 830) { // Si la pantalla se vuelve más pequeña de 830px
+            cambiar = true;
+        }
+
         if (ancho_ventana > 830) {
-            document.getElementById('hambMenu').setAttribute('class', 'hambMenu'); // Ocultamos el menu desplegable
+            if(cambiar) { // Comprobamos si tenemos que ocultar el menu desplegable
+                // Ocultamos el menu desplegable
+                document.getElementById('hambMenu').setAttribute('class', 'hambMenu'); // Ocultamos el menu desplegable
+                cambiar= false; // Ponemos false en nuestra variable bandera ya que no tenemos que realizar más cambios por el momento
+            }
+
         }
     });
 
